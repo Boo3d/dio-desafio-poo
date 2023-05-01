90076c90e6c5f41f6d84c1b1ff36ab9ab3325fe5
Dyego Henrique <dyego_henrique-boo@hotmail.com>
2023-05-01 02:52:55 -0300

    Interligando as classes de domínio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.Bootcamp;
 import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Dev;
 import br.com.dio.desafio.Mentoria;
 
 public class Main {
@@ -20,8 +22,44 @@ public class Main {
         mentoria.setDescricao("Descrição da mentoria de Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria); */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devDyego = new Dev();
+        devDyego.setNome("Dyego");
+        devDyego.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Incritos " + devDyego.getNome() + ": " + devDyego.getConteudosInscritos());
+
+        devDyego.progredir();
+        devDyego.progredir();
+        System.out.println("___");
+        
+        System.out.println("Conteudos Incritos " + devDyego.getNome() + ": " + devDyego.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos " + devDyego.getNome() + ": " + devDyego.getConteudosConcluidos());
+        System.out.println("XP: " + devDyego.calcularTotalXp());
+        
+        System.out.println("__________");
+        
+        Dev devAngelica = new Dev();
+        devAngelica.setNome("Angélica");
+        devAngelica.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Incritos " + devAngelica.getNome() + ": " + devAngelica.getConteudosInscritos());
+        
+        devAngelica.progredir();
+        devAngelica.progredir();
+        devAngelica.progredir();
+        System.out.println("___");
+        
+        System.out.println("Conteudos Incritos " + devAngelica.getNome() + ": " + devAngelica.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos " + devAngelica.getNome() + ": " + devAngelica.getConteudosConcluidos());
+        System.out.println("XP: " + devAngelica.calcularTotalXp());
     }
 }
